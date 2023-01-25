f26aa87d8d68b4f6d94691a532bcced09d60e281
Aryan Neizehbaz <aneizehbaz@gmail.com>
2023-01-26 00:47:46 +0330

    config issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,36 +1,18 @@
 /** @type {import('tailwindcss').Config} */
-const colors = require('tailwindcss/colors')
-const withMT = require("@material-tailwind/react/utils/withMT");
-module.exports = withMT({
+module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
-    "path-to-your-node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
-    "path-to-your-node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    colors:{
-      gold:'#d0a94d',
-      mainGray:'#303030',
-      bgGray:'#252525',
-      transparent: 'transparent',
-      current: 'currentColor',
-      black: colors.black,
-      white: colors.white,
-      gray: colors.gray,
-      emerald: colors.emerald,
-      indigo: colors.indigo,
-      yellow: colors.yellow,
-      mainGold: '#d0a94d',
-      dark: 'rgb(23, 26, 30)',
-      cardDark: '#252525',
-      footerColor: '#131212'
-    },
     extend: {
       fontWeight: {
         medium: 1000,
       },
       colors: {
-
+        'mainGold': '#d0a94d',
+        'dark': 'rgb(23, 26, 30)',
+        'cardDark': '#252525',
+        'footerColor': '#131212'
       },
       height: {
         '-100%': '-100%',
@@ -45,4 +27,4 @@ module.exports = withMT({
     },
   },
   plugins: [],
-});
+}
