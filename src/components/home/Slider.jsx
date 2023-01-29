c4067f9445c0c06e7c3a6c4dd1177324e26a45b7
Abolfazl <abolfazl30not30@gmail.com>
2023-01-29 23:55:43 +0330

    edit style of slider and text

--- src/components/home/Slider.jsx
+++ src/components/home/Slider.jsx
@@ -1,10 +1,25 @@
-import React, {useRef, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import "../../style/slider.css"
 import image1 from "../../img/1744263.jpg"
 import image2 from "../../img/bars-New-York-Assay-Office-US-Mint.webp"
 import image3 from "../../img/GettyImages-1130532216.jpg"
 
 const Slider = () => {
+
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+
+        //Implementing the setInterval method
+        const interval = setInterval(() => {
+            setCount(count + 1);
+            next()
+        }, 3000);
+
+        //Clearing the interval
+        return () => clearInterval(interval);
+    }, [count]);
+
     const carouselContainer = useRef(null)
     const [carouselData, setCarouselData] = useState([
         {
@@ -23,12 +38,12 @@ const Slider = () => {
     const [carouselInView, setCarouselInView] = useState([3, 2, 1])
     return (
         <>
-            <div className="flex flex-wrap">
+            <div className="flex flex-wrap justify-center">
                 <div className="w-[30%]">
                     <div className={'flex items-center lg:mt-0 mr-[50px] xsm:ml-5'}>
                         <div className="mt-20">
                             <h2 className={'font-bold text-white whitespace-nowrap md:text-[30px] mb-4 xsm:text-center xsm:text-[22px] xsm:mt-3'}>
-                                مطمئن ترین راه برای <span className={"text-gold"}>نگهداری طلا </span>شما
+                                مطمئن ترین راه برای <span className={"text-span text-gold"}>نگهداری طلا </span>شما
                             </h2>
                             <p className={'text-white md:text-[10px] leading-7 font-thin xsm:text-[13px]'}>
                                 اینجا یک متن درباره توضیحات وبسایت قرار میگیرد اینجا
@@ -40,37 +55,30 @@ const Slider = () => {
                         </div>
                     </div>
                 </div>
-                <div className="carousel">
+                <div className="carousel my-10">
                     <div className="carousel-container" ref={carouselContainer}>
                         {carouselData.map((item, index) => {
                             return (
-                                <div className={"slide"}>
-                                    <img src={item.src}
-                                         className={`carousel-item carousel-item-${carouselInView[index]}`}
-                                         data-index={`${index + 1}`} loading="lazy"/>
-                                </div>
-                           )
-                                    })}
-                                </div>
-                            <div className="carousel-controls">
-                                <button onClick={() => {
-                                    next()
-                                }} className="carousel-control carousel-control-next" data-name="next"></button>
-                            </div>
-                        </div>
-                        </div>
-                        </>
-                        )
+                                <img src={item.src}
+                                     className={`gradient-box carousel-item carousel-item-${carouselInView[index]}`}
+                                     data-index={`${index + 1}`} loading="lazy"/>
+                            )
+                        })}
+                    </div>
+                    <div className="carousel-controls">
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 
-                            function next() {
-                                // let updateDate = [...carouselData];
-                                // updateDate.push(updateDate.shift());
-                                // setCarouselData(updateDate)
+    function next() {
 
-                                let updateCarouselInView = [...carouselInView]
-                                updateCarouselInView.unshift(updateCarouselInView.pop());
-                                console.log(updateCarouselInView)
-                                setCarouselInView(updateCarouselInView)
-                            }
-                        }
-                            export default Slider;
+        let updateCarouselInView = [...carouselInView]
+        updateCarouselInView.unshift(updateCarouselInView.pop());
+        console.log(updateCarouselInView)
+        setCarouselInView(updateCarouselInView)
+
+    }
+}
+export default Slider;
