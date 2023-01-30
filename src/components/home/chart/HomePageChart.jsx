659d2c425649b28d34a826359ffc426ff1fa6aff
Abolfazl <abolfazl30not30@gmail.com>
2023-01-30 09:53:11 +0330

    delete unusage libraray

--- src/components/home/chart/HomePageChart.jsx
+++ src/components/home/chart/HomePageChart.jsx
@@ -1,9 +1,3 @@
-import {Box} from "@mui/material";
-import {TabContext, TabList} from "@mui/lab";
-import Tab from '@mui/material/Tab';
-import TabPanel from '@mui/lab/TabPanel';
-import Tabs from '@mui/material/Tabs';
-import {useEffect, useState} from "react";
 import Chart24 from "./chart24";
 import { Line } from 'react-chartjs-2';
 import { Chart as ChartJS } from 'chart.js/auto';
