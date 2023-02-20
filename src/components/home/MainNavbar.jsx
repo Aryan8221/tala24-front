f08b72bd1d15ab51cadc2c6fe55aed570a7bb7fb
Abolfazl <abolfazl30not30@gmail.com>
2023-02-21 01:34:27 +0330

    edit style of navbar

--- src/components/home/MainNavbar.jsx
+++ src/components/home/MainNavbar.jsx
@@ -3,7 +3,7 @@ import logo from "../../images/lastLogo.png"
 import { useState, useEffect } from "react";
 import {RxEnter} from "react-icons/rx"
 import "../../style/home.css"
-import {Link} from "react-router-dom"
+import {Link, NavLink} from "react-router-dom"
 
 import {
     Navbar,
@@ -48,24 +48,24 @@ const MainNavbar = () => {
                         <div className="hidden mr-8 w-full md:block md:w-auto" id="navbar-default">
                             <ul className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-transparent md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700" style={{backgroundColor:"transparent"}}>
                                 <li>
-                                    <a href="#"
-                                       className="block ml-7 py-2 pl-3 pr-4 text-white hover:text-gold rounded  md:active:text-gold md:bg-transparent md:text-blue-700 md:p-0 dark:text-white">خانه</a>
+                                    <NavLink to="/" activeClassName="active"
+                                       className="block ml-7 py-2 pl-3 pr-4 text-white hover:text-gold rounded   md:bg-transparent md:p-0 dark:text-white">خانه</NavLink>
                                 </li>
                                 <li>
-                                    <a href="#"
-                                       className="block py-2 pl-3 pr-4 text-white rounded hover:text-gold md:hover:bg-transparent md:hover:text-gold md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">خدمات</a>
+                                    <NavLink to="/" activeClassName="active"
+                                       className="block py-2 pl-3 pr-4 text-white rounded hover:text-gold md:hover:bg-transparent md:hover:text-gold md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">خدمات</NavLink>
                                 </li>
                                 <li>
-                                    <a href="#"
-                                       className="block py-2 pl-3 pr-4 text-white  rounded hover:text-gold md:hover:bg-transparent md:hover:text-gold md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">درباره ما</a>
+                                    <NavLink to="/" activeClassName="active"
+                                       className="block py-2 pl-3 pr-4 text-white  rounded hover:text-gold md:hover:bg-transparent md:hover:text-gold md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">درباره ما</NavLink>
                                 </li>
                                 <li>
-                                    <a href="#"
-                                       className="block py-2 pl-3 pr-4 text-white rounded hover:text-gold md:hover:bg-transparent md:hover:text-gold md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">تماس با ما</a>
+                                    <NavLink to="/" activeClassName="active"
+                                       className="block py-2 pl-3 pr-4 text-white rounded hover:text-gold md:hover:bg-transparent md:hover:text-gold md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">تماس با ما</NavLink>
                                 </li>
                                 <li>
-                                    <a href="#"
-                                       className="block py-2 pl-3 pr-4 text-white rounded hover:text-gold md:hover:bg-transparent md:hover:text-gold md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"> ورود به سامانه</a>
+                                    <NavLink to="/" activeClassName="active"
+                                       className="block py-2 pl-3 pr-4 text-white rounded hover:text-gold md:hover:bg-transparent md:hover:text-gold md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"> ورود به سامانه</NavLink>
                                 </li>
                             </ul>
                         </div>
